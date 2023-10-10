628636bb59897b48a696d94ef20888665ff1ff8f
progax01 <anurag35412@gmail.com>
2023-10-10 07:16:02 +0530

    updated changes

--- New Compound/sendeth.js
+++ New Compound/sendeth.js
@@ -2,7 +2,7 @@ const {Web3} = require('web3');
 const web3 = new Web3('http://127.0.0.1:8545'); // Connect to Ganache's RPC endpoint
 
 const fromAddress = '0x34952640e1e0F1F3cE82da047DCbf5B749c4687E'; // The address with Ether to send from
-const toAddress = '0xa0df350d2637096571F7A701CBc1C5fdE30dF76A'; // The unlocked account's address
+const toAddress = '0x075e72a5eDf65F0A5f44699c7654C1a76941Ddc8'; // The unlocked account's address
 const amountInEther = 500; // Amount of Ether to send (in Ether)
  
 web3.eth.sendTransaction({
--- Uniswap/git/TEST/sendeth.js
+++ Uniswap/git/TEST/sendeth.js
@@ -1,14 +1,14 @@
 const {Web3} = require('web3');
 const web3 = new Web3('http://127.0.0.1:8545'); // Connect to Ganache's RPC endpoint
 
-const fromAddress = '0xe6c8E997C1871B69516476e0cA290281f041037A'; // The address with Ether to send from (an unlocked account)
-const toAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // The address you want to send Ether to
-const amountInWei = web3.utils.toWei('1', 'ether'); // Amount of Ether to send (in Wei)
-
+const fromAddress = '0x34952640e1e0F1F3cE82da047DCbf5B749c4687E'; // The address with Ether to send from
+const toAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // The unlocked account's address
+const amountInEther = 500; // Amount of Ether to send (in Ether)
+ 
 web3.eth.sendTransaction({
   from: fromAddress,
   to: toAddress,
-  value: web3.utils.toHex(amountInWei),
+  value: web3.utils.toWei(amountInEther.toString(), 'ether'),
 })
 .on('transactionHash', (hash) => {
   console.log(`Transaction hash: ${hash}`);
